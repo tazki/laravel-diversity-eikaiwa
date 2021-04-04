3aaf925ba2a7ca5928883a30b1d4c87425f88610
Mark Bautista <tazki04@gmail.com>
2021-04-05 01:12:42 +0800

    Update About Us Company Details

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -46,12 +46,18 @@
 					<div class="company-info">
 						<h3>{{ __('Company Details') }}</h3>
 						<ul>
-							<li><strong>{{ __('Company Name') }}:</strong> DiversityEikaiwa</li>
-							<li><strong>{{ __('Postal Code') }}:</strong> 103-0023</li>
-							<li style="width:80%;"><strong>{{ __('Address') }}:</strong> {{ __('Level 7, Wakamatsu Building, 3-3-6 Nihonbashi-Honcho, Chuo-Ku, Tokyo 103-0023 Japan') }}</li>
-							<li><strong>{{ __('Phone') }}:</strong> <a href="tel://03-6202-7083">03 6202 7083</a></li>
+							<li><strong>{{ __('Business name') }}</strong><br /> DiversityEikaiwa</li>
+							<li style="width:80%;"><strong>{{ __('Address') }}</strong><br /> {{ __('Level 7, Wakamatsu Building, 3-3-6 Nihonbashi-Honcho, Chuo-Ku, Tokyo 103-0023 Japan') }}</li>
+							<li><strong>{{ __('Phone number') }}</strong><br /> <a href="tel://03-6202-7083">03 6202 7083</a></li>
+							<li><strong>{{ __('E-mail ') }}</strong><br /> <a href="mail:diversityeikaiwa2021@gmail.com">diversityeikaiwa2021@gmail.com</a></li>
+							<li><strong>{{ __('Business Representative') }}</strong><br /> Oliver Rivera</li>
+							<li><strong>{{ __('Price') }}</strong><br /> {{ __('Please check the price here:') }} <a href="{{ route('page_pricing') }}">{{ route('page_pricing') }}</a></li>
+							<li><strong>{{ __('Business Format') }}</strong><br /> {{ __('E-learning') }}</li>
+							<li><strong>{{ __('Mode of Payment') }}</strong><br /> {{ __('Credit card payment') }}</li>
+
+							{{-- <li><strong>{{ __('Postal Code') }}:</strong> 103-0023</li>
 							<li><strong>{{ __('Service start time') }}:</strong> {{ __('9:00 -17:00 (Monday -Friday)') }}</li>
-							<li><strong>{{ __('Other costs besides lesson charges') }}:</strong> {{ __('none') }}</li>
+							<li><strong>{{ __('Other costs besides lesson charges') }}:</strong> {{ __('none') }}</li> --}}
 						</ul>
 					</div>
 				</div>
@@ -59,11 +65,17 @@
 					<div class="company-info">
 						<h3>&nbsp;</h3>
 						<ul>
-							<li><strong>{{ __('President CEO') }}:</strong> Oliver Rivera</li>
-							<li><strong>{{ __('Company URL') }}:</strong> <a href="{{ route('page_home') }}">https://diversityeikaiwa.com</a></li>
-							<li><strong>{{ __('Content of Company Business') }}:</strong> {{ __('E-learning') }}</li>
-							<li><strong>{{ __('Type of Business') }}:</strong> {{ __('Education') }}</li>
-							<li><strong>{{ __('Return policies') }}:</strong> <a href="{{ route('page_terms') }}" target="_blank">{{ __('Terms and Condition') }}</a></li>
+							<li><strong>{{ __('Payment date') }}</strong><br /> {{ __('Credit card payment will be made at the time of application. The billing date depends on each card company.') }}</li>
+							<li><strong>{{ __('Fees other than the price and other customer burdens') }}</strong><br /> {{ __('None (However, Internet communication, gadgets such as PCs, tablets, smartphones, microphones, and Web cameras are provided by the student)') }}</li>
+							<li><strong>{{ __('About the service') }}</strong><br /> {{ __('All lesson are online and through Skype.') }}</li>
+							<li><strong>{{ __('When will the service will be delivered') }}</strong><br /> {{ __('In the case of credit card payment, the service will be available after we confirm the payment.') }}</li>							
+							<li><strong>{{ __('Refund/Withdrawal') }}</strong><br /> {{ __('If you wish to withdraw from the membership, please apply according to the procedures prescribed by our company. For details, see') }} <a href="{{ route('page_terms') }}" target="_blank">{{ __('Terms and Condition') }}</a></li>
+							<li><strong>{{ __('About the online lesson system and recommended environment') }}</strong><br /> {{ __('To use this service, use a computer, tablet, or smartphone connected to the Internet.') }}</li>
+							{{-- <li><strong>{{ __('President CEO') }}:</strong> Oliver Rivera</li>
+							<li><strong>{{ __('Company URL') }}:</strong> <a href="{{ route('page_home') }}">https://diversityeikaiwa.com</a></li> --}}
+							{{-- <li><strong>{{ __('Content of Company Business') }}:</strong> {{ __('E-learning') }}</li> --}}
+							{{-- <li><strong>{{ __('Type of Business') }}:</strong> {{ __('Education') }}</li>
+							<li><strong>{{ __('Return policies') }}:</strong> <a href="{{ route('page_terms') }}" target="_blank">{{ __('Terms and Condition') }}</a></li> --}}
 							{{-- <li><strong>{{ __('Number of Employees') }}:</strong> 5</li>
 							<li><strong>{{ __('Recent Annual Sales') }}:</strong> 0 yen</li>
 							<li><strong>{{ __('Company Foundation Date') }}:</strong> 2021/03/01</li>
@@ -77,6 +89,35 @@
 		</div>
 	</div>
 
+	<div class="fh5co-company">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-push-1 animate-box">
+					<div class="company-info">
+						<h3>{{ __('What are needed for the lesson?') }}</h3>
+						<ul>
+							<li>{{ __('Phone or Computer') }}</li>
+							<li>{{ __('Internet') }}</li>
+							<li>{{ __('Skype') }}</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-5 animate-box">
+					<div class="company-info">
+						<h3>{{ __('How to take a lesson?') }}</h3>
+						<ul>
+							<li><strong>{{ __('First') }}</strong><br /> {{ __('Download Skype.') }}</li>
+							<li><strong>{{ __('Second') }}</strong><br /> {{ __('Register an account.') }}</li>
+							<li><strong>{{ __('Third') }}</strong><br /> {{ __('Choose your teacher, time and date.') }}</li>
+							<li><strong>{{ __('Fourth') }}</strong><br /> {{ __('Take class on the day and time you booked.') }}</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	@php /*
 	<div class="fh5co-company">
 		<div class="container">
 			<div class="row">
@@ -102,6 +143,7 @@
 			</div>
 		</div>
 	</div>
+	*/ @endphp
 
 	{{-- <div id="fh5co-about">
 		<div class="container">
