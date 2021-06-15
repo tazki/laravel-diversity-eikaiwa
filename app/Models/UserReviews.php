008e0bd242460aea46415264bc01968ee40d168c
Mark Bautista <42196376+tazki@users.noreply.github.com>
2021-06-15 18:07:10 +0800

    Create user_reviews table

---
+++ app/Models/UserReviews.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserReviews extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
---
+++ database/migrations/2021_06_15_000001_create_user_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('student_id')->default(0);
+            $table->bigInteger('teacher_id')->default(0);
+            $table->string('review_title')->nullable();
+            $table->text('review_content')->nullable();
+            $table->integer('review_rating')->default(0);
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_reviews');
+    }
+}
